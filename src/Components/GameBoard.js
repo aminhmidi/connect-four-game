e9df8ef85f37e3b63281bd5b1e757405b5909019
Amin <aminhmidistudent@gmail.com>
2023-12-29 20:15:05 +0100

    added styling

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,21 +1,26 @@
 import React from "react";
 import GameCirlce from "./GameCirlce";
+import "../Game.css";
 
 const GameBoard = () => {
   return (
-    <div>
-      <GameCirlce id={1} value={5}>
-        Red
-      </GameCirlce>
-      <GameCirlce id={2} value={6}>
-        Blue
-      </GameCirlce>
-      <GameCirlce id={3}>Red</GameCirlce>
-      <GameCirlce id={4}>Blue</GameCirlce>
-      <GameCirlce id={5}>Red</GameCirlce>
-      <GameCirlce id={6}>Blue</GameCirlce>
-      <GameCirlce id={7}>Red</GameCirlce>
-      <GameCirlce id={8}>Blue</GameCirlce>
+    <div className="gameboard">
+      <GameCirlce id={1}></GameCirlce>
+      <GameCirlce id={2}></GameCirlce>
+      <GameCirlce id={3}></GameCirlce>
+      <GameCirlce id={4}></GameCirlce>
+      <GameCirlce id={5}></GameCirlce>
+      <GameCirlce id={6}></GameCirlce>
+      <GameCirlce id={7}></GameCirlce>
+      <GameCirlce id={8}></GameCirlce>
+      <GameCirlce id={9}></GameCirlce>
+      <GameCirlce id={10}></GameCirlce>
+      <GameCirlce id={11}></GameCirlce>
+      <GameCirlce id={12}></GameCirlce>
+      <GameCirlce id={13}></GameCirlce>
+      <GameCirlce id={14}></GameCirlce>
+      <GameCirlce id={15}></GameCirlce>
+      <GameCirlce id={16}></GameCirlce>
     </div>
   );
 };
--- src/Components/GameCirlce.js
+++ src/Components/GameCirlce.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "../Game.css";
 
-const onClick = (ev, id, value) => {
+const onClick = (ev, id) => {
   alert("on click" + id);
 };
 
-const GameCirlce = ({ id, value, children }) => {
-  console.log(id);
-  return <div onClick={(ev) => onClick(ev, id, value)}>{children}</div>;
+const GameCirlce = ({ id, children }) => {
+  return (
+    <div
+      className="gamecircle"
+      style={
+        id % 2 === 0 ? { backgroundColor: "red" } : { backgroundColor: "blue" }
+      }
+      onClick={(ev) => onClick(ev, id)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default GameCirlce;
