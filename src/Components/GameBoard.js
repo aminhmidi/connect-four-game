3e1ee4a3d1956e7172588ac0f4382f30546a084b
Amin <aminhmidistudent@gmail.com>
2024-01-11 12:44:23 +0100

    game functioning

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GameCirlce from "./GameCirlce";
 import "../Game.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import { isWinner } from "../helper";
-
-const NUM_CIRCLES = 16;
-const NO_PLAYER = 0;
-const PLAYER_1 = 1;
-const PLAYER_2 = 2;
+import { isDraw, isWinner } from "../helper";
+import {
+  GAME_STATE_PLAYING,
+  GAME_STATE_WIN,
+  NO_PLAYER,
+  PLAYER_1,
+  PLAYER_2,
+  NUM_CIRCLES,
+  GAME_STATE_DRAW,
+} from "../Constants";
 
 const GameBoard = () => {
   const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+  const [gameState, setGameState] = useState(GAME_STATE_PLAYING);
+  const [winPlayer, setWinPlayer] = useState(NO_PLAYER);
   console.log(gameBoard);
+  useEffect(() => {
+    initGame();
+  }, []);
+  const initGame = () => {
+    console.log("init game");
+    setGameBoard(Array(16).fill(NO_PLAYER));
+    setCurrentPlayer(PLAYER_1);
+  };
   const initBoard = () => {
     const circles = [];
     for (let i = 0; i < NUM_CIRCLES; i++) {
@@ -22,9 +36,16 @@ const GameBoard = () => {
     return circles;
   };
   const circleClicked = (id) => {
-    console.log("circle clicke :" + id);
+    console.log("circle clicked :" + id);
+    if (gameBoard[id] !== NO_PLAYER) return;
+    if (gameState !== GAME_STATE_PLAYING) return;
     if (isWinner(gameBoard, id, currentPlayer)) {
-      console.log("WINNER");
+      setGameState(GAME_STATE_WIN);
+      setWinPlayer(currentPlayer);
+    }
+    if (isDraw(gameBoard, id, currentPlayer)) {
+      setGameState(GAME_STATE_DRAW);
+      setWinPlayer(NO_PLAYER);
     }
     setGameBoard((prev) => {
       return prev.map((circle, pos) => {
@@ -48,9 +69,13 @@ const GameBoard = () => {
   };
   return (
     <>
-      <Header player={currentPlayer} />
+      <Header
+        gameState={gameState}
+        currentPlayer={currentPlayer}
+        winPlayer={winPlayer}
+      />
       <div className="gameboard">{initBoard()}</div>
-      <Footer />
+      <Footer onClickEvent={initGame} />
     </>
   );
 };
